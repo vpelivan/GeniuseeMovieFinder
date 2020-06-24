5f0faaf122a64a0635926509e6b8aa0d58b09854
Victor Pelivan <pelivan.v.1988@gmail.com>
2020-06-24 21:57:16 +0300

    Started a project

---
+++ GeniuseeMovieFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeniuseeMovieFinder
+//
+//  Created by Victor Pelivan on 24.06.2020.
+//  Copyright © 2020 Viktor Pelivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
